b3873ec63b1889fadc2f2e11bbc670020705e540
Md. Jubaer Hossain <jubaercoding@gmail.com>
2025-06-09 13:44:48 +0600

    updated authentication and worked on login history

--- app/Http/Controllers/Auth/AuthenticatedSessionController.php
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\Auth\LoginRequest;
+use App\Models\LoginHistory;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -11,6 +12,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 use Inertia\Response;
+use Stevebauman\Location\Facades\Location;
 
 class AuthenticatedSessionController extends Controller
 {
@@ -33,6 +35,15 @@ class AuthenticatedSessionController extends Controller
         $request->authenticate();
 
         $request->session()->regenerate();
+        $user = Auth::user();
+        LoginHistory::create([
+            'user_id'    => $user->id,
+            'ip_address' => $request->ip(),
+            'user_agent' => $request->userAgent(),
+            'login_at'   => now(),
+            'status'     => true,
+            'location'   => $this->getLocation($request->ip()), // optional
+        ]);
 
         return redirect()->intended(RouteServiceProvider::HOME);
     }
@@ -42,6 +53,17 @@ class AuthenticatedSessionController extends Controller
      */
     public function destroy(Request $request): RedirectResponse
     {
+
+        $user = $request->user();
+
+        LoginHistory::where('user_id', $user->id)
+            ->where('status', true)
+            ->latest()
+            ->first()?->update([
+                'logout_at' => now(),
+                'status' => false
+            ]);
+
         Auth::guard('web')->logout();
 
         $request->session()->invalidate();
@@ -49,5 +71,17 @@ class AuthenticatedSessionController extends Controller
         $request->session()->regenerateToken();
 
         return redirect('/');
+    }
+
+
+    protected function getLocation($ip)
+    {
+        $location = Location::get($ip);
+
+        if ($location) {
+            return $location->city . ', ' . $location->countryName;
+        }
+
+        return $ip;
     }
 }
--- app/Models/LoginHistory.php
+++ app/Models/LoginHistory.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class LoginHistory extends Model
 {
     use HasFactory;
+
+    protected $guarded = ['id'];
 }
